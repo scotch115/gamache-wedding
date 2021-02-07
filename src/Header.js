6e168c4766b0e1555a7a54c9e3c576988bc17421
scotch115 <jordangamache115@gmail.com>
2021-02-06 22:43:52 -0500

    Linked new photos page

--- src/Header.js
+++ src/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
                 <li><a href="/travel">Travel</a></li>
                 <li><a href="/menu">Menu</a></li>
                 <li><a href="https://www.zola.com/registry/jordantorri">Registry</a></li>
-                <li><a href="/photos">Photos</a></li>
+                <li><a href="https://www.zola.com/wedding/jordantorri/photo">Photos</a></li>
                 </ul>
             </div>
          );
